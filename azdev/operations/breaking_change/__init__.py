533388ec56be36227f034673cfe0fe8d77404455
qinkaiwu <qinkaiwu@microsoft.com>
2024-04-29 13:38:49 +0800

    Add license header

--- azdev/operations/breaking_change/__init__.py
+++ azdev/operations/breaking_change/__init__.py
@@ -1,3 +1,8 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -----------------------------------------------------------------------------
 from collections import defaultdict
 from importlib import import_module
 import packaging.version
